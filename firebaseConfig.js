c50b4ef56456a7f5114637cd686802864cb741e2
zefparis <ben.barere@gmail.com>
2024-11-05 22:14:45 +0100

    Configuration initiale Firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC715AcapEi1U_8zjMf5iwsmfw0POOnYv8",
+  authDomain: "autordc.firebaseapp.com",
+  projectId: "autordc",
+  storageBucket: "autordc.firebasestorage.app",
+  messagingSenderId: "480236207256",
+  appId: "1:480236207256:web:b7fe2e4fd91013d15f24ed"
+};
+  
